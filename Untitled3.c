c41ff11cc8cffdac4eadfa5a7e4070ec92b939fd
LakhalEngineering <125914042+LakhalEngineering@users.noreply.github.com>
2023-10-13 19:53:37 +0200

    Using Extern keyword
    Attention to this commend

---
+++ Untitled3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern int displayCount;
+
+int main()
+{
+    int a;
+    printf("Totale log messages: %d\n", displayCount);
+}
---
+++ Untitled4.c
@@ -0,0 +1,11 @@
+#include "Untitled4.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int displayCount = 0;
+
+int poo( int a, int b){
+    printf(" displayCount ", displayCount);
+    return a + b;
+}
